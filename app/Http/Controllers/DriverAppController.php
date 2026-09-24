015a36203a39e8d3f4fc8f15ef3d40e4900d4c43
Lei0919 <leiannjudeadico20@gmail.com>
2026-09-24 08:08:28 +0800

    fix: resolve Pint formatting issues

--- app/Http/Controllers/DriverAppController.php
+++ app/Http/Controllers/DriverAppController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\DriverApp;
-use Inertia\Inertia;
 use App\Http\Requests\StoreDriverAppRequest;
+use App\Models\DriverApp;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class DriverAppController extends Controller
 {
@@ -24,10 +24,7 @@ class DriverAppController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
-        //
-    }
+    public function create() {}
 
     /**
      * Store a newly created resource in storage.
@@ -56,24 +53,15 @@ class DriverAppController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
-    {
-        //
-    }
+    public function edit(string $id) {}
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
-    {
-
-    }
+    public function update(Request $request, string $id) {}
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
-    {
-        //
-    }
+    public function destroy(string $id) {}
 }
